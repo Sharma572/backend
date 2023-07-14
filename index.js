9675de94648b3a4ba1fed63347263ee48971f6d5
Raunak Sharma <98183441+Sharma572@users.noreply.github.com>
2023-07-14 11:44:38 +0530

    error handler with constant file as middleware

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use("/contact", require("./routes/contactRoute"))
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port 👌 ${PORT}`);
